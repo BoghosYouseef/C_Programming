daf34df77a4e51dcd057755ba72a3bfb2d5b8d08
Boghos Youseef <43525406+BoghosYouseef@users.noreply.github.com>
2019-12-10 15:42:08 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,26 @@
+  
 /******************************************************************************
-
-
                             Online C Compiler.
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
-
 *******************************************************************************/
 
 #include <stdio.h>
 #include <string.h> 
 
 char* to_lower_case(char* s){
+    
+
     for(int i = 0; i != '\n'; i++){
+
         if((s[i] >= 'A') && (s[i] <= 'Z')){
+        	
             s[i] += 32;
         }
+            
     }
+     
+    
     return s;
 }
 
@@ -39,25 +44,28 @@ int gcd(int a, int b){
 }
 
 
-int[30] factor (int n){
-    int[30] factors_list;
-    for(int i = 0; i = n, i++){
-        int num = 0
-        if ((i*i)>= n){
-            factors_list[num]= i
-            num += 1
-        }
-    }
-    for (int i = 0; i>= 30, i++){
-        if(factors_list[i] )
-    }
 
-int main()
-{
-    char* string = "HelLO WoRLd";
-    printf("%s\n", string);
-    // char* new_string = to_lower_case(string);
-    // printf("%s\n", new_string);
-    printf("%d",gcd(12, 15));
-    return 0;
+int factor(int n){
+    
+    char [] factors_list;
+    for(int i = 0; i = n; i++){
+        int num = 0;
+        int factor_element = i;
+        while ((factor*factor)<= n){
+            num += 1;
+            factor_element *= i;
+        }
+        strcpy(factors_list, ("(%d, %d)",factor , num));
+    
+    }
+    return factors_list;
+}
+
+
+int main(){
+	char msg [] = "HeLlOWoRLD";
+	printf("%s \n", &msg);
+	printf("%s \n", to_lower_case(&msg));
+	printf("%d \n", gcd(150,120));
+	
 }
