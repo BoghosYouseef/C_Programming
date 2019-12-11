4f39fbb7786fbf008fdcc68125f481655a91404f
Boghos Youseef <43525406+BoghosYouseef@users.noreply.github.com>
2019-12-11 11:23:40 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -37,28 +37,61 @@ int gcd(int a, int b){
         a = b;
         b = c;
         c = a % b;
-        if (c = 1){
+        if (c == 1){
             return b;
         }
     }
+
+    return "no GCD found";
 }
 
 
 
+
+
 int factor(int n){
-    
-    char [] factors_list;
-    for(int i = 0; i = n; i++){
+
+    int sum = n;
+    for(int i = 2; i != n; i++){
         int num = 0;
-        int factor_element = i;
-        while ((factor*factor)<= n){
+        while ( sum % i == 0){
             num += 1;
-            factor_element *= i;
+            sum /= i;
+            
+
         }
-        strcpy(factors_list, ("(%d, %d)",factor , num));
-    
+        if(num != 0){
+            printf("(%d, %d)",i , num);
+        }
     }
-    return factors_list;
+    
+        
+}
+
+int binary_decomp(int n){
+    while(n > 0){
+        printf("%d", n%2);
+        n /= 2;
+    }
+}
+
+int sieve_of_eratosthenes(int n){
+    int lista [n];
+    for (int i = 0; i < n; i++ ){
+        lista [i] = 0;
+    }
+    for (int j = 2; j < n; j++){
+        for(int k = 0; k < n; k++){
+            if(j*k < n){
+            lista [j*k] ++;
+            }
+        }
+    }
+    for (int l = 0; l < n; l++){
+        if(lista[l] == 0){
+        printf("%d " , l);
+        }
+    }
 }
 
 
@@ -66,6 +99,18 @@ int main(){
 	char msg [] = "HeLlOWoRLD";
 	printf("%s \n", &msg);
 	printf("%s \n", to_lower_case(&msg));
-	printf("%d \n", gcd(150,120));
-	
+	//printf("%d \n", gcd(150,120));
+    factor(20);
+    printf("\n");
+    binary_decomp(18);
+    printf("\n");
+    long int numpow = 1;
+    for(int i = 0; i <= 33; i++){
+        binary_decomp(numpow);
+        printf(":");
+        printf("%d \n", numpow);
+        
+        numpow *= 2;
+    }
+    sieve_of_eratosthenes(100);
 }
